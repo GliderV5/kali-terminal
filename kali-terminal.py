d0e8dfe69ca30ce08a521003742ea241390386e9
GliderV5 <60002998+GliderV5@users.noreply.github.com>
2023-08-10 17:35:30 +0200

    Update kali-terminal.py

--- kali-terminal.py
+++ kali-terminal.py
@@ -2,9 +2,10 @@ import os
 
 os.system("clear")
 if str(input("This tool will install zsh and configure it like kali terminal. Continue ? (y/n) >>> ")) == "y":
-    os.system("sudo apt-get install zsh -y")
+    os.system("sudo apt-get install zsh")
     os.system("sudo cp .zshrc ~/.zshrc")
     os.system("dconf load /org/gnome/terminal/ < gnome_terminal_settings_backup.txt")
+    os.system("chsh -s $(which zsh)")
     os.system("clear")
     print()
     if str(input("The system need to reboot. Reboot now ? (y/n) >>> ")) == "y":
