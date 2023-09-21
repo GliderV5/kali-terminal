1d08ea43f69eeeb17008b98ac6bd143abae5c9c7
GliderV5 <60002998+GliderV5@users.noreply.github.com>
2023-09-21 14:39:08 +0200

    Update kali-terminal.py

--- kali-terminal.py
+++ kali-terminal.py
@@ -5,7 +5,7 @@ if str(input("This tool will install zsh and configure it like kali terminal. Co
     os.system("sudo apt-get install zsh")
     os.system("sudo cp .zshrc ~/.zshrc")
     os.system("dconf load /org/gnome/terminal/ < gnome_terminal_settings_backup.txt")
-    os.system("chsh -s $(which zsh)")
+    #os.system("chsh -s $(which zsh)")
     os.system("clear")
     print()
     os.system("mkdir ~/.config/zsh/")
@@ -13,7 +13,7 @@ if str(input("This tool will install zsh and configure it like kali terminal. Co
     os.system("git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.config/zsh/zsh-syntax-highlighting")
     os.system("git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/zsh-autosuggestions")
     os.system("git clone https://github.com/zap-zsh/sudo ~/.config/zsh/sudo-zsh")
-    os.system("source ~/.zshrc")
+    #os.system("source ~/.zshrc")
     if str(input("The system need to reboot. Reboot now ? (y/n) >>> ")) == "y":
         os.system("sudo reboot")
     else:
